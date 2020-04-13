8a4923ecfe273b0bb77eed4558e20c9e7c8b76fb
Arya Murthi <aryamurthi@Aryas-MBP.fios-router.home>
2020-04-13 16:33:12 -0400

    loading speed updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,12 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+
+    {resolve: `gatsby-plugin-s3`,
+      options: {
+          bucketName: 'aryamurthi.info'
+      },
+    },
     'gatsby-plugin-sass',
   ],
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import { Icon, InlineIcon } from '@iconify/react';
+import { InlineIcon } from '@iconify/react';
 import eagleIcon from '@iconify/icons-noto-v1/eagle';
 
 
