e8672ab653221bf7a36c4570dfc399f938842c25
Arya Murthi <aryamurthi@Aryas-MBP.fios-router.home>
2020-04-13 18:11:49 -0400

    speed improvements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright"> ©2020 Arya Murthi  Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+        <p className="copyright"> ©2020 Arya Murthi  Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a> helmet icon design by <a href="https://thenounproject.com/Graphic_Tigers/">Graphic Tigers</a> </p>
+  
     </footer>
 )
 
